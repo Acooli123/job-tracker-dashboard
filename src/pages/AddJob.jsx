09fb5d8965fca49be7edc201478aa7bcb7509c4e
Acooli123 <soumyajitacooli@gmail.com>
2026-06-23 21:01:33 +0530

    home page design

--- src/pages/AddJob.jsx
+++ src/pages/AddJob.jsx
@@ -2,7 +2,7 @@ import JobForm from "../components/JobForm";
 
 export default function AddJob() {
   return (
-    <div className="p-4 sm:p-6 md:p-8">
+    <div className="p-4 sm:p-6 md:p-8 ">
       <JobForm />
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,21 @@
 export default function Home() {
   return (
-    <div className="text-center mt-16 sm:mt-20">
-      <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold">
-        Job Tracker Dashboard
-      </h1>
+    <div
+  className="h-screen w-full fixed bg-cover bg-center flex items-center justify-center"
+  style={{
+    backgroundImage:
+      "url('https://cdn.dribbble.com/userupload/15224257/file/original-768b4689e07e9b0e6fb0fa4b631cccf0.jpg?resize=400x0')",
+  }}
+>
+  <div className="bg-black/50 w-full h-full flex flex-col justify-center items-center">
+    <h1 className="text-5xl font-bold text-white">
+      Job Tracker Dashboard
+    </h1>
 
-      <p className="mt-4 sm:mt-5 text-gray-600 text-base sm:text-lg">
-        Track all your job applications easily.
-      </p>
-    </div>
+    <p className="text-gray-200 mt-4">
+      Track all your job applications easily.
+    </p>
+  </div>
+</div>
   );
 }
